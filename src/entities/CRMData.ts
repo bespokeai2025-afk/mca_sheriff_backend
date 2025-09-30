bfc9b853fd84415637bd32c74011819280ab5f84
Savita444 <savita.hajare@sumagoinfotech.com>
2025-09-30 16:30:21 +0530

    add email

--- src/entities/CRMData.ts
+++ src/entities/CRMData.ts
@@ -26,8 +26,9 @@ export class CRMData /* extends Common */ {
     @Column({ type: 'text', nullable: true })
     email: string;
 
-    @Column({ type: 'text', nullable: false })
+    @Column({ type: 'text', nullable: true })
     email_id: string;
+
 
     @Column({ type: "boolean", default: true})
     need_to_call: boolean;
