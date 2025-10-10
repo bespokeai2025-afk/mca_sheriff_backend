962cb76ed4f767bb7b22815df99309319f903d7a
Sumago-Git <shubhamkothavade@sumagoinfotech.in>
2025-10-10 10:47:48 +0530

    cron job changes for crone job fixes for front end new

--- src/utils/scheduler.ts
+++ src/utils/scheduler.ts
@@ -164,7 +164,7 @@ export class CallScheduler {
           console.error("❌ Error fetching leads:", error.message);
         }
       },
-      { timezone: "Asia/Kolkata" } //  Run cron in UTC
+      { timezone: "UTC" } //  Run cron in UTC
     );
 
     this.scheduledJobs.set(id, job);
