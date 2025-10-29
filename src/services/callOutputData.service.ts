0d26e19c78581896a27c764fdc73c4dae32aa957
Savita444 <savita.hajare@sumagoinfotech.com>
2025-10-29 19:12:47 +0530

    add in return 2 fields in admin login api

--- src/services/callOutputData.service.ts
+++ src/services/callOutputData.service.ts
@@ -257,16 +257,15 @@ export class callOutputDataService {
   // To get Count Of User Call
   public async getUserCallDataCount(verifyUser: any, pageSize: number, currentPage: number) {
     try {
-      let whereCondition: any = {
-      isDeleted: false,
-      clear_all_data: false,
-    };
-      if (verifyUser.user_exist) {
-        whereCondition = { isActive: true, isDeleted: false, clear_all_data: false };
-      }
-      if (verifyUser.admin_exist) {
-        whereCondition = { isDeleted: false };
-      }
+      
+      // let whereCondition: any = {};
+       const whereCondition: any = { isActive: true, isDeleted: false, clear_all_data: false };
+      // if (verifyUser.user_exist) {
+      //   whereCondition = { isActive: true, isDeleted: false, clear_all_data: false };
+      // }
+      // if (verifyUser.admin_exist) {
+      //   whereCondition = { isDeleted: false };
+      // }
 
       // Total calls
       const totalCall = await this.callOutputRepository.count({ where: whereCondition });
